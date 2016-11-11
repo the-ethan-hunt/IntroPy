10e76e80eb1272b94b4c1ca834d8f3c43345e338
Dhruv Apte <dhruvgirishapte@gmail.com>
2016-11-11 18:16:03 +0530

    anagram.py
    An anagram program in Python

---
+++ anagram.py
@@ -0,0 +1,19 @@
+#To find if a word is an anagram
+def isAnagram(original,test):
+    if len(original) ==len(test):
+        count= [0]* ord('z')
+        for c in original:
+            count[ord(c)] +=1
+            for c in test:
+                if count[ord(c)]==0:
+                    return False
+                else:
+                    count[ord(c)]-=1
+                    return True
+                return False
+            original= raw_input("enter first dtring:")
+            test= raw_input("enter second string:")
+            print
+            print ("%s and %s are %s") %(original,test,"anagrams" if (isAnagram(original, test)) else "not anagrams")
+            
+            
